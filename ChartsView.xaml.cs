59a785da403d4ec47767b93622815f8da1eece06
Pitigoi <lascuzeanu_stefan@yahoo.com>
2023-01-15 15:04:07 +0200

    done

--- ChartsView.xaml.cs
+++ ChartsView.xaml.cs
@@ -23,7 +23,6 @@ namespace login
     /// </summary>
     public partial class ChartsView : UserControl
     {
-        public long MinVal;
         public Func<double,string> XFormatter = value =>  new DateTime((long)value).ToString("g");
         public Func<double,string> YFormatter = value => ((decimal)value).ToString("c");
         public ChartsView()
@@ -31,14 +30,15 @@ namespace login
             InitializeComponent();
             ChartInitializer();
             DataContext = this;
-            MinVal= 0;
 
             var dayConfig = Mappers.Xy<Tuple<DateTime, decimal>>()
                           .X(dayModel => (double)dayModel.Item1.Ticks)
                           .Y(dayModel => ((double)dayModel.Item2));
 
             LiveCharts.Charting.For<Tuple<DateTime, string>>(dayConfig);
+            chart.Series=new SeriesCollection(dayConfig);
             //axisx;
+            axisx.LabelFormatter = XFormatter;
             axisy.LabelFormatter = YFormatter;
             dateend.SelectedDate = datestart.SelectedDate = DateTime.Now.Date;
         }
@@ -59,17 +59,21 @@ namespace login
             DateTime dc1 =(DateTime)datestart.SelectedDate;
             DateTime dc2 = ((DateTime)dateend.SelectedDate).AddDays(1);
 
-            var at = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dc1 && p.time <= dc2 select new { time=p.time,amount=Math.Abs(p.amount) }).AsEnumerable().Select(p => new Tuple<DateTime,decimal>(p.time, p.amount));
-            var bt = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dc1 && i.time <= dc2 select new { time = i.time, amount = i.amount }).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
+            var at = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dc1 && p.time <= dc2 select new { p.time,amount=Math.Abs(p.amount) }).OrderByDescending(s => s.time).AsEnumerable().Select(p => new Tuple<DateTime,decimal>(p.time, p.amount));
+            var bt = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dc1 && i.time <= dc2 select new {  i.time,  i.amount }).OrderByDescending(s => s.time).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
             this.chart.Series.Clear();
+            axisx.MinValue = dc1.Ticks;
+            axisx.MaxValue = dc2.Ticks;
             if (at.Count() > 0)
             {
-                MinVal = at.AsEnumerable().Select(s => s.Item1.Ticks).Min();
                 this.chart.Series.Add(new LineSeries
                 {
                     Values = new ChartValues<Tuple<DateTime, decimal>>(at),
                     Name = "Payments"
                 });
+            }
+            if (bt.Count() > 0)
+            {
                 this.chart.Series.Add(new LineSeries
                 {
                     Values = new ChartValues<Tuple<DateTime, decimal>>(bt),
@@ -81,41 +85,55 @@ namespace login
         private void Today_Click(object sender, RoutedEventArgs e)
         {
             PayContext c = new PayContext();
-            DateTime dt = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            DateTime dt = DateTime.Now.Date;
 
-            var at = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dt select Math.Abs(p.amount)).ToList();
-            var bt = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dt select i.amount).ToList();
+            var at = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dt select new {  p.time, amount = Math.Abs(p.amount) }).OrderByDescending(s => s.time).AsEnumerable().Select(p => new Tuple<DateTime, decimal>(p.time, p.amount));
+            var bt = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dt select new { i.time,  i.amount }).OrderByDescending(s => s.time).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
             this.chart.Series.Clear();
-            this.chart.Series.Add(new LineSeries
+            axisx.MinValue = dt.Ticks;
+            if (at.Count() > 0)
             {
-                Values = new ChartValues<decimal>(at),
-                Name = "Payments"
-            });
-            this.chart.Series.Add(new LineSeries
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(at),
+                    Name = "Payments"
+                });
+            }
+            if (bt.Count() > 0)
             {
-                Values = new ChartValues<decimal>(bt),
-                Name = "Incomes"
-            });
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(bt),
+                    Name = "Incomes"
+                });
+            }
         }
 
         private void Week_Click(object sender, RoutedEventArgs e)
         {
             PayContext c = new PayContext();
-            DateTime dw = DateTime.Now.Date.AddDays((1 - (int)DateTime.Now.DayOfWeek) % 7);
+            DateTime dw = DateTime.Now.Date.AddDays((((int)DateTime.Now.DayOfWeek+6)%7)*-1);
 
-            var aw = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dw select Math.Abs(p.amount)).ToList();
-            var bw = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dw select i.amount).ToList();
+            var aw = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dw select new { p.time, amount = Math.Abs(p.amount) }).OrderByDescending(s => s.time).AsEnumerable().Select(p => new Tuple<DateTime, decimal>(p.time, p.amount));
+            var bw = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dw select new { i.time, i.amount }).OrderByDescending(s => s.time).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
             this.chart.Series.Clear();
-            this.chart.Series.Add(new LineSeries
+            axisx.MinValue = dw.Ticks;
+            if (aw.Count() > 0)
             {
-                Values = new ChartValues<decimal>(aw),
-                Name = "Payments"
-            });
-            this.chart.Series.Add(new LineSeries
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(aw),
+                    Name = "Payments"
+                });
+            }
+            if (bw.Count() > 0)
             {
-                Values = new ChartValues<decimal>(bw),
-                Name = "Incomes"
-            });
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(bw),
+                    Name = "Incomes"
+                });
+            }
         }
 
         private void Month_Click(object sender, RoutedEventArgs e)
@@ -123,19 +141,26 @@ namespace login
             PayContext c = new PayContext();
             DateTime dl = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
 
-            var al = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dl select Math.Abs(p.amount)).ToList();
-            var bl = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dl select i.amount).ToList();
+            var al = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= dl select new { p.time, amount = Math.Abs(p.amount) }).OrderByDescending(s => s.time).AsEnumerable().Select(p => new Tuple<DateTime, decimal>(p.time, p.amount));
+            var bl = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= dl select new { i.time, i.amount }).OrderByDescending(s => s.time).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
             this.chart.Series.Clear();
-            this.chart.Series.Add(new LineSeries
+            axisx.MinValue = dl.Ticks;
+            if (al.Count() > 0)
             {
-                Values = new ChartValues<decimal>(al),
-                Name = "Payments"
-            });
-            this.chart.Series.Add(new LineSeries
-            {
-                Values = new ChartValues<decimal>(bl),
-                Name = "Incomes"
-            });
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(al),
+                    Name = "Payments"
+                });
+            }
+            if (bl.Count() > 0)
+            { 
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(bl),
+                    Name = "Incomes"
+                });
+            }
         }
 
         private void Year_Click(object sender, RoutedEventArgs e)
@@ -143,19 +168,26 @@ namespace login
             PayContext c= new PayContext();
             DateTime da = new DateTime(DateTime.Now.Year, 1, 1);
 
-            var aa = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= da select Math.Abs(p.amount)).ToList();
-            var ba = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= da select i.amount).ToList();
+            var aa = (from p in c.Payments where p.uid == PayContext.currentId && p.time >= da select new { p.time, amount = Math.Abs(p.amount) }).OrderByDescending(s => s.time).AsEnumerable().Select(p => new Tuple<DateTime, decimal>(p.time, p.amount));
+            var ba = (from i in c.Incomes where i.uid == PayContext.currentId && i.time >= da select new { i.time, i.amount }).OrderByDescending(s => s.time).AsEnumerable().Select(i => new Tuple<DateTime, decimal>(i.time, i.amount));
             this.chart.Series.Clear();
-            this.chart.Series.Add(new LineSeries
+            axisx.MinValue = da.Ticks;
+            if (aa.Count() > 0)
             {
-                Values = new ChartValues<decimal>(aa),
-                Name = "Payments"
-            });
-            this.chart.Series.Add(new LineSeries
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(aa),
+                    Name = "Payments"
+                });
+            }
+            if (ba.Count() > 0)
             {
-                Values = new ChartValues<decimal>(ba),
-                Name = "Incomes"
-            });
+                this.chart.Series.Add(new LineSeries
+                {
+                    Values = new ChartValues<Tuple<DateTime, decimal>>(ba),
+                    Name = "Incomes"
+                });
+            }
         }
     }
 }
